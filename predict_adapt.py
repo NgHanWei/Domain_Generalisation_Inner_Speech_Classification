6f4bc9ea122ba964853ac128639c5b2086e68acf
NgHanWei <82133534+NgHanWei@users.noreply.github.com>
2022-11-02 04:17:33 +0800

    Add files via upload

--- predict_adapt.py
+++ predict_adapt.py
@@ -27,6 +27,8 @@ import json
 import logging
 import sys
 
+from sklearn.utils import shuffle
+
 from Inner_Speech_Dataset.Python_Processing.Data_extractions import  Extract_data_from_subject
 from Inner_Speech_Dataset.Python_Processing.Data_processing import  Select_time_window, Transform_for_classificator, Split_trial_in_time
 
@@ -46,7 +48,11 @@ parser = argparse.ArgumentParser(
     description='Subject-adaptative classification with Inner Speech')
 parser.add_argument('-subj', type=int,
                     help='Target Subject', required=True)
+parser.add_argument('-coeff', type=float,
+                    help='Data Selection Threshold Multiplier', default=1.5)
 parser.add_argument('--meta',default=False, help='Training Mode', action='store_true')
+parser.add_argument('--dgtrain',default=False, help='Domain Generalisation on Training', action='store_true')
+parser.add_argument('--dgtest',default=False, help='Domain Generalisation on Test', action='store_true')
 args = parser.parse_args()
 
 ### Hyperparameters
@@ -65,6 +71,9 @@ t_end = 3.5
 # Target Subject
 targ_subj = args.subj #[1 to 10]
 meta = args.meta
+dgtrain = args.dgtrain
+dgtest = args.dgtest
+coeff = args.coeff
 
 X_train = np.array([])
 Y_train = np.array([])
@@ -111,10 +120,11 @@ for i in range(1,11):
     subjs = [1,2,3,4,5,6,7,8,9,10]
 
     if i == targ_subj:
-        X_test = X[100:]
-        Y_test = Y[100:]
-        X_adapt = X[:100]
-        Y_adapt = Y[:100]
+        X, Y = shuffle(X, Y, random_state=0)
+        X_test = X[:]
+        Y_test = Y[:]
+        X_adapt = X[:50]
+        Y_adapt = Y[:50]
     elif i == subjs[targ_subj-2]:
         X_val = X[:].astype(np.float32)
         Y_val = Y[:].astype(np.int64)
@@ -126,13 +136,14 @@ for i in range(1,11):
 
 
 print("Training Data shape")    
-print(X_train.shape)
+
 original_trials = X_train.shape[0]
-print(Y_train.shape)
+original_X_train = X_train
 
 print("Test Data shape")
 print(X_test.shape)
 print(Y_test.shape)
+print(np.unique(Y_test))
 
 X_train = X_train.astype(np.float32)
 Y_train = Y_train.astype(np.int64)
@@ -154,11 +165,8 @@ X_test = X_test[:,np.newaxis,:,:].to('cuda')
 
 X_adapt = torch.from_numpy(X_adapt)
 X_adapt = X_adapt[:,np.newaxis,:,:].to('cuda')
-
-
-# X_train = []
-# X_valid = []
-# X_test = []
+print(X_train.shape)
+print(Y_train.shape)
 
 # VAE model
 input_shape=(X_adapt.shape[1:])
@@ -169,9 +177,7 @@ features = 16
 data = 'eeg'
 clip = 0
 
-targ_subj = 1
-
-data_load = torch.split(X_train,batch_size)
+data_load = torch.split(X_test[:50],batch_size)
 data_load_check = torch.split(X_train,1)
 channels = len(X_train[0,0,:,0])
 
@@ -226,6 +232,7 @@ def fit(model):
     # For each batch
     for batch in tqdm(range(0,len(data_load))):
         optimizer.zero_grad()
+        # print(data_load[batch].shape)
         reconstruction, mu, logvar = model(data_load[batch])
         bce_loss = recon_loss(reconstruction,data_load[batch])
         loss = final_loss(bce_loss, mu, logvar)
@@ -295,11 +302,19 @@ for epoch in range(epochs):
 model = vanilla_vae.VanillaVAE(filters=filters,channels=channels,features=features,data_type=data,data_length=len(data_load[0][:,0,0,0])).to(device)
 model.load_state_dict(torch.load(file_name))
 
+data_load = torch.split(X_test,len(X_test))
+model.eval()
+with torch.no_grad():
+    reconstruction, mu, logvar = model(data_load[0])
+mu = mu.cpu().detach().numpy()
+print(mu.shape)
+# plot_clustering(mu, Y_test, engine='matplotlib', download = False)
+
 model.eval()
 with torch.no_grad():
     # print(X_test[:1].shape)
-    reconstruction, mu, logvar = model(X_test[:1])
-    test_bce_loss = recon_loss(reconstruction,X_test[:1])
+    reconstruction, mu, logvar = model(X_test[:50])
+    test_bce_loss = recon_loss(reconstruction,X_test[:50])
     print(test_bce_loss)
 
     total_bce = 0
@@ -315,17 +330,24 @@ with torch.no_grad():
         # print(batch_bce_loss)
         total_bce += batch_bce_loss
 
-        if batch_bce_loss < test_bce_loss*1.5:
+        if batch_bce_loss < test_bce_loss*coeff:
             print(batch_bce_loss)
             anomaly_list.append(count)
-            # X_train_new = np.concatenate((X_train_new, reconstruction.detach().cpu().numpy()),axis=0) if X_train_new != [] else reconstruction.detach().cpu().numpy()
-            # Y_train_new = np.concatenate((Y_train_new, [Y_train[count]]),axis=0) if Y_train_new != [] else [Y_train[count]]
-            X_train_new = np.concatenate((X_train_new, data_load_check[batch].detach().cpu().numpy()),axis=0) if X_train_new != [] else data_load_check[batch].detach().cpu().numpy()
-            Y_train_new = np.concatenate((Y_train_new, [Y_train[count]]),axis=0) if Y_train_new != [] else [Y_train[count]]
+            if dgtrain == True:
+                X_train_new = np.concatenate((X_train_new, reconstruction.detach().cpu().numpy()),axis=0) if X_train_new != [] else reconstruction.detach().cpu().numpy()
+                Y_train_new = np.concatenate((Y_train_new, [Y_train[count]]),axis=0) if Y_train_new != [] else [Y_train[count]]
+            else:
+                X_train_new = np.concatenate((X_train_new, data_load_check[batch].detach().cpu().numpy()),axis=0) if X_train_new != [] else data_load_check[batch].detach().cpu().numpy()
+                Y_train_new = np.concatenate((Y_train_new, [Y_train[count]]),axis=0) if Y_train_new != [] else [Y_train[count]]
 
         count = count + 1
 
+    if X_train_new == []:
+        X_train_new = reconstruction.detach().cpu().numpy()
+        Y_train_new = np.asarray([Y_train[count-1]])
+
     X_train_new = X_train_new[:,0,:,:]
+
     print(X_train_new.shape)
     print(Y_train_new.shape)
 
@@ -345,7 +367,6 @@ with torch.no_grad():
 
 
 ### Hyperparameters
-
 # The root dir have to point to the folder that cointains the database
 root_dir = "D:/Imagined_speech/"
 
@@ -406,10 +427,9 @@ for i in range(1,11):
     subjs = [1,2,3,4,5,6,7,8,9,10]
 
     if i == targ_subj:
-        X_test = X[100:]
-        Y_test = Y[100:]
-        X_adapt = X[:100]
-        Y_adapt = Y[:100]
+        X, Y = shuffle(X, Y, random_state=0)
+        X_test = X[50:]
+        Y_test = Y[50:]
     elif i == subjs[targ_subj-2]:
         X_val = X[:].astype(np.float32)
         Y_val = Y[:].astype(np.int64)
@@ -428,12 +448,20 @@ print("Test Data shape")
 print(X_test.shape)
 print(Y_test.shape)
 
+# If too many data is cut
+# if X_train_new.shape[0] < 500:
+#     print("TOO MANY CUT")
+#     X_train = X_train.astype(np.float32)
+#     Y_train = Y_train.astype(np.int64)
+# else:
 Max_val = 500
 norm = np.amax(abs(X_train_new))
 X_train_new = Max_val * X_train_new/norm
 X_train = X_train_new.astype(np.float32)
 Y_train = Y_train_new.astype(np.int64)
+
 print(Y_train.shape)
+
 X_val = X_val.astype(np.float32)
 Y_val = Y_val.astype(np.int64)
 X_test = X_test.astype(np.float32)
@@ -466,9 +494,15 @@ new_X_val = Max_val * new_X_val/norm
 TRAIN_EPOCH = 200
 BATCH_SIZE = 16
 
+# print(X_train)
+# print(X_val)
+# print(X_test)
+
 train_set = SignalAndTarget(X_train, y=Y_train)
-valid_set = SignalAndTarget(X_val, y=Y_val)
-# valid_set = SignalAndTarget(new_X_val[:,0,:,:], y=Y_val)
+if dgtest == True:
+    valid_set = SignalAndTarget(new_X_val[:,0,:,:], y=Y_val)
+else:
+    valid_set = SignalAndTarget(X_val, y=Y_val)
 test_set = SignalAndTarget(X_test, y=Y_test)
 n_classes = 4
 in_chans = train_set.X.shape[1]
@@ -491,9 +525,9 @@ base_model_param = {
     'loss': rememberer.lowest_val
 }
 torch.save(base_model_param, pjoin(
-    './results/', 'model_subj{}.pt'.format(targ_subj)))
+    './results/', 'DG_model_subj{}.pt'.format(targ_subj)))
 model.epochs_df.to_csv(
-    pjoin('./results/', 'epochs_subj{}.csv'.format(targ_subj)))
+    pjoin('./results/', 'DG_epochs_subj{}.csv'.format(targ_subj)))
 
 
 new_X = torch.from_numpy(test_set.X)
@@ -505,7 +539,11 @@ Max_val = 500
 norm = np.amax(abs(new_X))
 new_X = Max_val * new_X/norm
 print(new_X.shape)
-# test_set = SignalAndTarget(new_X[:,0,:,:], y=Y_test)
+
+# Comment out to not apply recon on test
+if dgtest == True:
+    test_set = SignalAndTarget(new_X[:,0,:,:], y=Y_test)
+
 test_loss = model.evaluate(test_set.X, test_set.y)
 print(test_loss)
 with open(pjoin('./results/', 'test_base_subj{}.json'.format(targ_subj)), 'w') as f:
---
+++ train_adapt.py
@@ -0,0 +1,351 @@
+#!/usr/bin/env python
+# coding: utf-8
+'''Subject-adaptative classification with KU Data,
+using Deep ConvNet model from [1].
+
+References
+----------
+.. [1] Schirrmeister, R. T., Springenberg, J. T., Fiederer, L. D. J.,
+   Glasstetter, M., Eggensperger, K., Tangermann, M., Hutter, F. & Ball, T. (2017).
+   Deep learning with convolutional neural networks for EEG decoding and
+   visualization.
+   Human Brain Mapping , Aug. 2017. Online: http://dx.doi.org/10.1002/hbm.23730
+'''
+import argparse
+import json
+import logging
+import sys
+from os.path import join as pjoin
+
+import numpy as np
+import torch
+import torch.nn.functional as F
+from braindecode.models.deep4 import Deep5Net
+from braindecode.torch_ext.optimizers import AdamW
+from braindecode.torch_ext.util import set_random_seeds
+from torch import nn
+from vae_models import vanilla_vae
+import torch.optim as optim
+from sklearn.utils import shuffle
+
+from Inner_Speech_Dataset.Python_Processing.Data_extractions import  Extract_data_from_subject
+from Inner_Speech_Dataset.Python_Processing.Data_processing import  Select_time_window, Transform_for_classificator, Split_trial_in_time
+
+# python train_adapt.py -scheme 5 -trfrate 10 -subj $subj
+
+logging.basicConfig(format='%(asctime)s %(levelname)s : %(message)s',
+                    level=logging.INFO, stream=sys.stdout)
+torch.manual_seed(0)
+torch.cuda.manual_seed_all(0)
+np.random.seed(0)
+torch.backends.cudnn.deterministic = True
+set_random_seeds(seed=2022, cuda=True)
+
+parser = argparse.ArgumentParser(
+    description='Subject-adaptative classification with Inner Speech')
+parser.add_argument('--meta',default=False, help='Training Mode', action='store_true')
+parser.add_argument('-scheme', type=int, help='Adaptation scheme', default=5)
+parser.add_argument(
+    '-trfrate', type=int, help='The percentage of data for adaptation', default=100)
+parser.add_argument('-lr', type=float, help='Learning rate', default=0.0005)
+parser.add_argument('-gpu', type=int, help='The gpu device to use', default=0)
+parser.add_argument('-subj', type=int,
+                    help='Target Subject for Subject Selection', required=True)
+
+args = parser.parse_args()
+outpath = './adapt_results/'
+modelpath = './recon_1.5/'
+scheme = args.scheme
+rate = args.trfrate
+lr = args.lr
+meta = args.meta
+torch.cuda.set_device(args.gpu)
+set_random_seeds(seed=2022, cuda=True)
+BATCH_SIZE = 16
+TRAIN_EPOCH = 30
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+batch_size = 16
+kernel_size = 5
+filters = 8
+features = 16
+data = 'eeg'
+
+root_dir = "D:/Imagined_speech/"
+
+# Data Type
+datatype = "EEG"
+
+# Sampling rate
+fs = 256
+
+# Select the useful par of each trial. Time in seconds
+t_start = 1.5
+t_end = 3.5
+
+all_loss = 0
+for subj in range(1,11):
+
+    # Target Subject
+    targ_subj = subj #[1 to 10]
+
+    X_train = np.array([])
+    Y_train = np.array([])
+    for i in range(1,11):
+
+        # Subject number
+        N_S = i   #[1 to 10]
+
+        #@title Data extraction and processing
+
+        # Load all trials for a sigle subject
+        X, Y = Extract_data_from_subject(root_dir, N_S, datatype)
+
+        # Cut usefull time. i.e action interval
+        X = Select_time_window(X = X, t_start = t_start, t_end = t_end, fs = fs)
+
+        # print("Data shape: [trials x channels x samples]")
+        # print(X.shape) # Trials, channels, samples
+
+        # print("Labels shape")
+        # print(Y.shape) # Time stamp, class , condition, session
+
+        # Conditions to compared
+        Conditions = [["Inner"],["Inner"],["Inner"],["Inner"]]
+        # The class for the above condition
+        Classes    = [  ["Up"] ,["Down"],["Left"],["Right"] ]
+
+        # Transform data and keep only the trials of interes
+        X , Y =  Transform_for_classificator(X, Y, Classes, Conditions)
+
+        print("Final data shape")
+        print(X.shape)
+
+        print("Final labels shape")
+        print(Y.shape)
+
+        # Normalize Data
+        Max_val = 500
+        norm = np.amax(abs(X))
+        X = Max_val * X/norm
+
+        if i == targ_subj:
+            print(X.shape)
+            # p = np.random.permutation(len(Y))
+            # X = X[p]
+            # Y = Y[p]
+            X, Y = shuffle(X, Y, random_state=0)
+            X_test = X
+            Y_test = Y
+        else:
+            X_train = np.concatenate((X_train, X),axis=0) if len(X_train) > 0 else X
+            Y_train = np.concatenate((Y_train, Y),axis=0) if len(Y_train) > 0 else Y
+
+    print("Training Data shape")    
+    print(X_train.shape)
+    print(Y_train.shape)
+
+    print("Test Data shape")
+    print(X_test.shape)
+    print(Y_test.shape)
+
+    X_train2 = X_train.astype(np.float32)
+    Y_train2 = Y_train.astype(np.int64)
+    X_test = X_test.astype(np.float32)
+    Y_test = Y_test.astype(np.int64)
+
+    channels = len(X_train[0,:,0])
+    file_name = "./trained_vae/vae_torch" +  '_' + str(data)  + '_' + str(targ_subj) + '_' + str(filters) + '_' + str(channels) + '_' + str(features) + ".pt"
+
+    n_classes = 4
+    in_chans = X.shape[1]
+    # final_conv_length = auto ensures we only get a single output in the time dimension
+    model = Deep5Net(in_chans=in_chans, n_classes=n_classes,
+                    input_time_length=X.shape[2],
+                    final_conv_length='auto').cuda()
+
+    # Deprecated.
+
+
+    def reset_conv_pool_block(network, block_nr):
+        suffix = "_{:d}".format(block_nr)
+        conv = getattr(network, 'conv' + suffix)
+        kernel_size = conv.kernel_size
+        n_filters_before = conv.in_channels
+        n_filters = conv.out_channels
+        setattr(network, 'conv' + suffix,
+                nn.Conv2d(
+                    n_filters_before,
+                    n_filters,
+                    kernel_size,
+                    stride=(1, 1),
+                    bias=False,
+                ))
+        setattr(network, 'bnorm' + suffix,
+                nn.BatchNorm2d(
+                    n_filters,
+                    momentum=0.1,
+                    affine=True,
+                    eps=1e-5,
+                ))
+        # Initialize the layers.
+        conv = getattr(network, 'conv' + suffix)
+        bnorm = getattr(network, 'bnorm' + suffix)
+        nn.init.xavier_uniform_(conv.weight, gain=1)
+        nn.init.constant_(bnorm.weight, 1)
+        nn.init.constant_(bnorm.bias, 0)
+
+
+    def reset_model(checkpoint):
+        # Load the state dict of the model.
+        model.network.load_state_dict(checkpoint['model_state_dict'])
+
+        # # Resets the last conv block
+        # reset_conv_pool_block(model.network, block_nr=4)
+        # reset_conv_pool_block(model.network, block_nr=3)
+        # reset_conv_pool_block(model.network, block_nr=2)
+        # # Resets the fully-connected layer.
+        # # Parameters of newly constructed modules have requires_grad=True by default.
+        # n_final_conv_length = model.network.conv_classifier.kernel_size[0]
+        # n_prev_filter = model.network.conv_classifier.in_channels
+        # n_classes = model.network.conv_classifier.out_channels
+        # model.network.conv_classifier = nn.Conv2d(
+        #     n_prev_filter, n_classes, (n_final_conv_length, 1), bias=True)
+        # nn.init.xavier_uniform_(model.network.conv_classifier.weight, gain=1)
+        # nn.init.constant_(model.network.conv_classifier.bias, 0)
+
+        if scheme != 5:
+            # Freeze all layers.
+            for param in model.network.parameters():
+                param.requires_grad = False
+
+            if scheme in {1, 2, 3, 4}:
+                # Unfreeze the FC layer.
+                for param in model.network.conv_classifier.parameters():
+                    param.requires_grad = True
+
+            if scheme in {2, 3, 4}:
+                # Unfreeze the conv4 layer.
+                for param in model.network.conv_4.parameters():
+                    param.requires_grad = True
+                for param in model.network.bnorm_4.parameters():
+                    param.requires_grad = True
+
+            if scheme in {3, 4}:
+                # Unfreeze the conv3 layer.
+                for param in model.network.conv_3.parameters():
+                    param.requires_grad = True
+                for param in model.network.bnorm_3.parameters():
+                    param.requires_grad = True
+
+            if scheme == 4:
+                # Unfreeze the conv2 layer.
+                for param in model.network.conv_2.parameters():
+                    param.requires_grad = True
+                for param in model.network.bnorm_2.parameters():
+                    param.requires_grad = True
+
+        # Only optimize parameters that requires gradient.
+        optimizer = AdamW(filter(lambda p: p.requires_grad, model.network.parameters()),
+                        lr=lr, weight_decay=0.5*0.001)
+        # optimizer.load_state_dict(checkpoint['optimizer_state_dict'])
+        model.compile(loss=F.nll_loss, optimizer=optimizer,
+                    iterator_seed=2022, )
+
+    cutoff = int(rate * 50 / 100)
+    # Use only session 1 data for training
+    assert(cutoff <= 50)
+
+    total_loss = []
+
+    checkpoint = torch.load(pjoin(modelpath, 'DG_model_subj' + str(targ_subj) + '.pt'),
+                            map_location='cuda:' + str(args.gpu))
+    reset_model(checkpoint)
+
+    X, Y = X_test, Y_test
+    X_train, Y_train = X[:cutoff], Y[:cutoff]
+
+    model_vae = vanilla_vae.VanillaVAE(filters=filters,channels=channels,features=features,data_type=data,data_length=len(X_train[:,0,0])).to(device)
+    model_vae.load_state_dict(torch.load(file_name))
+    optimizer = optim.Adam(model.parameters(), lr=lr,betas=(0.5, 0.999),weight_decay=0.5*lr)
+
+    X_new = torch.from_numpy(X_train)
+    X_new = X_new[:,np.newaxis,:,:].to('cuda')
+    data_load = torch.split(X_new,batch_size)
+
+    def recon_loss(outputs,targets):
+
+        outputs = torch.flatten(outputs)
+        targets = torch.flatten(targets)
+
+        loss = nn.MSELoss()
+
+        recon_loss = loss(outputs,targets)
+
+        return recon_loss
+
+    def final_loss(bce_loss, mu, logvar):
+        BCE = bce_loss 
+        KLD = -0.5 * torch.sum(1 + logvar - mu.pow(2) - logvar.exp())
+        return BCE + KLD
+
+    def fit(model):
+        model.train()
+        running_loss = 0.0
+        # For each batch
+        for batch in range(0,len(data_load)):
+            optimizer.zero_grad()
+            reconstruction, mu, logvar = model(data_load[batch])
+            bce_loss = recon_loss(reconstruction,data_load[batch])
+            loss = final_loss(bce_loss, mu, logvar)
+            running_loss += loss.item()
+            loss.backward()
+            optimizer.step()
+
+        train_loss = running_loss/len(X_train)
+
+        return train_loss
+
+    epochs = 20
+    for epoch in range(epochs):
+        print(f"Epoch {epoch+1} of {epochs}")
+        train_epoch_loss = fit(model_vae)
+
+    new_X = torch.from_numpy(X_train)
+    new_X = new_X[:,np.newaxis,:,:].to('cuda')
+    model_vae.eval()
+    with torch.no_grad():
+        reconstruction, mu, logvar = model_vae(new_X)
+    new_X = reconstruction.detach().cpu().numpy()
+    Max_val = 500
+    norm = np.amax(abs(new_X))
+    new_X = Max_val * new_X/norm
+    # X_train= new_X[:,0,:,:]
+
+    X_val, Y_val = X[cutoff:], Y[cutoff:]
+    # X_val, Y_val = X, Y
+    model.fit(X_train, Y_train, epochs=TRAIN_EPOCH,
+                batch_size=BATCH_SIZE, scheduler='cosine',
+                validation_data=(X_val, Y_val), remember_best_column='valid_misclass',meta=meta)
+    model.epochs_df.to_csv(pjoin(outpath, 'epochs' + str(targ_subj) + '.csv'))
+
+    new_X = torch.from_numpy(X_test)
+    new_X = new_X[:,np.newaxis,:,:].to('cuda')
+    model_vae.eval()
+    with torch.no_grad():
+        reconstruction, mu, logvar = model_vae(new_X)
+    new_X = reconstruction.detach().cpu().numpy()
+    Max_val = 500
+    norm = np.amax(abs(new_X))
+    new_X = Max_val * new_X/norm
+    # X_test= new_X[:,0,:,:]
+
+    test_loss = model.evaluate(X_test[cutoff:], Y_test[cutoff:])
+    total_loss.append(test_loss["misclass"])
+    with open(pjoin(outpath, 'test' + str(targ_subj) + '.json'), 'w') as f:
+        json.dump(test_loss, f)
+
+    print(total_loss[0])
+    all_loss += total_loss[0]
+
+print(all_loss/10)
